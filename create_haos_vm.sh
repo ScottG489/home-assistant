841ddd79d9c298dd30faeef2bedf3815d48dcd33
Scott Giminiani <scottg489@gmail.com>
2021-11-23 02:42:27 -0500

    Use vboxmanage to set up HAOS VM
    This way we don't have to deal with vagrant. Everything possible with
    vagrant is possible with vboxmanage with less magic going on. VBox has
    great documentation:
    https://www.virtualbox.org/manual/ch08.html

---
+++ create_haos_vm.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Note that bridgeadapter may differ
+
+# Note that latest version may differ
+wget https://github.com/home-assistant/operating-system/releases/download/6.6/haos_ova-6.6.vmdk.zip
+unzip haos_ova-6.6.vmdk.zip
+
+VM_UUID=$(vboxmanage createvm --name haos-vm --default --register --ostype Linux_64 | grep UUID | awk '{print $2}')
+vboxmanage modifyvm $VM_UUID --firmware efi --nic1 bridged --bridgeadapter1 enp0s25 --memory 4096
+vboxmanage storageattach $VM_UUID --storagectl IDE --port 0 --device 0 --type hdd --medium haos_ova-6.6.vmdk
+vboxmanage startvm $VM_UUID
+
+#vboxmanage showvminfo --machinereadable $VM_UUID --details 
+#vboxmanage unregistervm $VM_UUID --delete
