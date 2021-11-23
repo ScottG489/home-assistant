c60a693612e676a903ac014d867fd6f897364931
Scott Giminiani <scottg489@gmail.com>
2021-11-23 03:23:07 -0500

    Start VM in headless mode

--- create_haos_vm.sh
+++ create_haos_vm.sh
@@ -8,7 +8,7 @@ unzip haos_ova-6.6.vmdk.zip
 VM_UUID=$(vboxmanage createvm --name haos-vm --default --register --ostype Linux_64 | grep UUID | awk '{print $2}')
 vboxmanage modifyvm $VM_UUID --firmware efi --nic1 bridged --bridgeadapter1 enp0s25 --memory 4096
 vboxmanage storageattach $VM_UUID --storagectl IDE --port 0 --device 0 --type hdd --medium haos_ova-6.6.vmdk
-vboxmanage startvm $VM_UUID
+vboxmanage startvm --type headless $VM_UUID
 
 #vboxmanage showvminfo --machinereadable $VM_UUID --details 
 #vboxmanage unregistervm $VM_UUID --delete
