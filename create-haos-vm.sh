5a3a82792fbdeef58c2b435ee3eb65e725e26e1f
Scott Giminiani <scottg489@gmail.com>
2021-12-01 19:40:34 -0500

    Dynamically get the correct network device to use for the bridge
    This may not be reliable, but it should dynamically get the correct
    network device to use for the VM's bridge adapter.

--- create-haos-vm.sh
+++ create-haos-vm.sh
@@ -7,7 +7,8 @@ wget --progress=bar:force:noscroll https://github.com/home-assistant/operating-s
 unzip haos_ova-6.6.vmdk.zip
 
 VM_UUID=$(vboxmanage createvm --name haos-vm --default --register --ostype Linux_64 | grep UUID | awk '{print $2}')
-vboxmanage modifyvm $VM_UUID --firmware efi --nic1 bridged --bridgeadapter1 enp0s25 --memory 4096
+NET_DEVICE=$(ip link | grep -v 'lo\|vir\|wl\|^[^0-9]' | awk '{print $2}' | sed 's/.$//')
+vboxmanage modifyvm $VM_UUID --firmware efi --nic1 bridged --bridgeadapter1 "$NET_DEVICE" --memory 4096
 vboxmanage storageattach $VM_UUID --storagectl IDE --port 0 --device 0 --type hdd --medium haos_ova-6.6.vmdk
 vboxmanage startvm --type headless $VM_UUID
 
