32ba32c658f07402ff2cb0e7b9cde65d2c4a15b3
Scott Giminiani <scottg489@gmail.com>
2021-12-02 22:36:33 -0500

    Don't start VM from script
    In the ansible playbook that's using this, I'm setting up a service to
    manage the VM; particularly to start the VM on boot. I think this script
    is better suited to just create the VM anyways rather than start it too.

--- create-haos-vm.sh
+++ create-haos-vm.sh
@@ -10,7 +10,7 @@ VM_UUID=$(vboxmanage createvm --name haos-vm --default --register --ostype Linux
 NET_DEVICE=$(ip link | grep -v 'lo\|vir\|wl\|^[^0-9]' | awk '{print $2}' | sed 's/.$//')
 vboxmanage modifyvm "$VM_UUID" --firmware efi --nic1 bridged --bridgeadapter1 "$NET_DEVICE" --memory 4096
 vboxmanage storageattach "$VM_UUID" --storagectl IDE --port 0 --device 0 --type hdd --medium haos_ova-6.6.vmdk
-vboxmanage startvm --type headless "$VM_UUID"
 
-#vboxmanage showvminfo --machinereadable $VM_UUID --details 
+#vboxmanage startvm --type headless "$VM_UUID"
+#vboxmanage showvminfo --machinereadable $VM_UUID --details
 #vboxmanage unregistervm $VM_UUID --delete
