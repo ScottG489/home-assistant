16134f893cce8089d55857ad693acc40b7ae4079
Scott Giminiani <scottg489@gmail.com>
2021-12-06 19:37:33 -0500

    Attach Bluetooth device to VM
    The BT device needs to be attached to the VM. We use BT for various
    things in Home Assistant so it's necessary for it to be available to the
    VM.
    
    This makes the assumption that there will be only one USB device
    detected and that it will be for the BT device. So far on two separate
    computers this has been true so we'll stick with this for now.

--- create-haos-vm.sh
+++ create-haos-vm.sh
@@ -8,8 +8,10 @@ unzip haos_ova-6.6.vmdk.zip
 
 VM_UUID=$(vboxmanage createvm --name haos-vm --default --register --ostype Linux_64 | grep UUID | awk '{print $2}')
 NET_DEVICE=$(ip link | grep -v 'lo\|vir\|wl\|^[^0-9]' | awk '{print $2}' | sed 's/.$//')
+BLUETOOTH_USB_DEVICE_UUID=$(VBoxManage list -l usbhost | grep UUID | awk '{print $2}')
 vboxmanage modifyvm "$VM_UUID" --firmware efi --nic1 bridged --bridgeadapter1 "$NET_DEVICE" --memory 4096
 vboxmanage storageattach "$VM_UUID" --storagectl IDE --port 0 --device 0 --type hdd --medium haos_ova-6.6.vmdk
+vboxmanage controlvm "$VM_UUID" usbattach "$BLUETOOTH_USB_DEVICE_UUID"
 
 #vboxmanage startvm --type headless "$VM_UUID"
 #vboxmanage showvminfo --machinereadable $VM_UUID --details
